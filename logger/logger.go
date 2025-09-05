ba3208693a60f28790e3031d33614532d12bc7a5
3086953492 <3086953492@qq.com>
2025-09-05 11:22:27 +0800

    feat(logger): 添加LogError函数用于记录错误日志
    添加LogError函数以统一记录错误日志的格式，包含函数名、操作、时间戳和错误信息等字段，便于错误追踪和分析

--- logger/logger.go
+++ logger/logger.go
@@ -1,8 +1,10 @@
 package logger
 
 import (
-	"go.uber.org/zap"
 	"sync"
+	"time"
+
+	"go.uber.org/zap"
 )
 
 var (
@@ -82,4 +84,16 @@ func Error(msg string, fields ...zap.Field) {
 
 func GetLogger() *zap.Logger {
 	return Default().GetLogger()
+}
+
+func LogError(funcName, operation, message string, err error, fields ...zap.Field) {
+	baseFields := []zap.Field{
+		zap.String("function", funcName),
+		zap.String("operation", operation),
+		zap.Time("timestamp", time.Now()),
+		zap.Error(err),
+	}
+
+	allFields := append(baseFields, fields...)
+	Error(message, allFields...)
 }
