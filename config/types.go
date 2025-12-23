8b957919ac3f38ae62a01a737a917b20119fed79
3086953492 <3086953492@qq.com>
2025-12-23 17:15:24 +0800

    refactor(config): 移除未使用的OAuth配置及相关类型定义
    删除config/types.go中的OAuth字段及config/types/oauth.go文件，因该功能已不再使用

--- config/types.go
+++ config/types.go
@@ -8,7 +8,6 @@ type Config struct {
 	Database   types.DatabaseConfig   `json:"database" yaml:"database" mapstructure:"database"`
 	Redis      types.RedisConfig      `json:"redis" yaml:"redis" mapstructure:"redis"`
 	AuthToken  types.AuthTokenConfig  `json:"auth_token" yaml:"auth_token" mapstructure:"auth_token"`
-	OAuth      types.OAuthConfig      `json:"oauth" yaml:"oauth" mapstructure:"oauth"`
 	Log        types.LogConfig        `json:"log" yaml:"log" mapstructure:"log"`
 	Middleware types.MiddlewareConfig `json:"middleware" yaml:"middleware" mapstructure:"middleware"`
 	AliyunOSS  types.AliyunOSSConfig  `json:"aliyun_oss" yaml:"aliyun_oss" mapstructure:"aliyun_oss"`
