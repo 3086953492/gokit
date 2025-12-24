bf38f7419a4ef8fc9124783224c27855ddd7c7fb
3086953492 <3086953492@qq.com>
2025-12-24 15:30:44 +0800

    feat(goauth): 添加访问令牌和刷新令牌的密钥配置
    在 Goauth 配置中新增 access_token_secret 和 refresh_token_secret 字段，用于增强认证安全性

--- config/types/goauth.go
+++ config/types/goauth.go
@@ -1,9 +1,11 @@
 package types
 
 type GoauthConfig struct {
-	FrontendBaseURL string `json:"frontend_base_url" yaml:"frontend_base_url" mapstructure:"frontend_base_url"`
-	BackendBaseURL  string `json:"backend_base_url" yaml:"backend_base_url" mapstructure:"backend_base_url"`
-	ClientID        string `json:"client_id" yaml:"client_id" mapstructure:"client_id"`
-	ClientSecret    string `json:"client_secret" yaml:"client_secret" mapstructure:"client_secret"`
-	RedirectURI     string `json:"redirect_uri" yaml:"redirect_uri" mapstructure:"redirect_uri"`
+	FrontendBaseURL    string `json:"frontend_base_url" yaml:"frontend_base_url" mapstructure:"frontend_base_url"`
+	BackendBaseURL     string `json:"backend_base_url" yaml:"backend_base_url" mapstructure:"backend_base_url"`
+	ClientID           string `json:"client_id" yaml:"client_id" mapstructure:"client_id"`
+	ClientSecret       string `json:"client_secret" yaml:"client_secret" mapstructure:"client_secret"`
+	RedirectURI        string `json:"redirect_uri" yaml:"redirect_uri" mapstructure:"redirect_uri"`
+	AccessTokenSecret  string `json:"access_token_secret" yaml:"access_token_secret" mapstructure:"access_token_secret"`
+	RefreshTokenSecret string `json:"refresh_token_secret" yaml:"refresh_token_secret" mapstructure:"refresh_token_secret"`
 }
