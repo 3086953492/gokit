9115e1706e58d12e79ce93e35bd565e4a7f9faa6
3086953492 <3086953492@qq.com>
2025-12-27 15:13:46 +0800

    feat(配置): 在示例配置中添加域名字段
    - 在 config.json 示例文件中新增 domain 字段，设置为 localhost
    - 在 config.yaml 示例文件中新增 domain 字段，设置为 localhost
    - 在 server.go 文件中更新 ServerConfig 结构体，添加 Domain 字段以支持域名配置

--- config/types/server.go
+++ config/types/server.go
@@ -6,4 +6,5 @@ type ServerConfig struct {
 	FrontendURL string `json:"frontend_url" yaml:"frontend_url" mapstructure:"frontend_url"`
 	Mode        string `json:"mode" yaml:"mode" mapstructure:"mode"`
 	ID          int    `json:"id" yaml:"id" mapstructure:"id"`
+	Domain      string `json:"domain" yaml:"domain" mapstructure:"domain"`
 }
